d274d85716a8ad4d146a845cae962da8fc94fa04
Mateusz-Adamczak <161976386+Mateusz-Adamczak@users.noreply.github.com>
2025-05-29 21:11:12 +0200

    Connection between client and server

--- chatroom/chat-client.cpp
+++ chatroom/chat-client.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 #include <thread>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-void receiveMessages() {
+#define PORT 12345
+
+
+void receiveMessages(int socket_fd) {
     std::cout<<"(Message receiver placeholder)\n";
 }
 
 int main() {
-    // Connect to server here
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in server_address{};
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
+    
+    if (connect(socket_fd, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+        std::cout<<"Connection to chat server failed.\n";
+        return 1;
+    }
+    
     std::cout<<"Connected to chat server."<<std::endl;
     
     std::string nickname;
@@ -14,7 +30,7 @@ int main() {
     std::cin>>nickname;
     std::cout<<"You are now chatting as \""<<nickname<<"\".\n";
     
-    std::thread receiver(receiveMessages);
+    std::thread receiver(receiveMessages, socket_fd);
     
     std::string msg;
     while (true) {
@@ -29,6 +45,7 @@ int main() {
         std::cout<<msg; // temp
     }
     
+    close(socket_fd);
     receiver.join();
     
     return 0;
